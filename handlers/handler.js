55799619fd98aba3ebb189ac4371f3fcfe601605
Bertie <berniesnell@gmail.com>
2019-02-27 19:46:56 +0000

    Adding initial serverless deploy script

---
+++ handlers/handler.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const handler = () => {
+  console.log('BLAWDOKAWD');
+  console.log('awd')
+  return [
+    {
+      title: 'test',
+      content: 'test',
+      createdAt: '23432341221',
+    }
+  ]  
+};
+
+
+module.exports = {
+  handler,
+};
