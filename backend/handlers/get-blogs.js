336dbe65602abd18f5df20ddd330eab8a8074de5
lukemurray77 <lukemurray737@googlemail.com>
2019-03-02 14:46:01 +0000

    go inital setup

--- backend/handlers/get-blogs.js
+++ backend/handlers/get-blogs.js
@@ -1,11 +1,21 @@
 'use strict';
 
-const handler = () => {
+const handler = async () => {
   console.log('2');
   console.log('awd')
   return [
     {
       title: 'test',
+      content: 'test',
+      createdAt: '23432341221',
+    },
+    {
+      title: 'test1',
+      content: 'test',
+      createdAt: '23432341221',
+    },
+    {
+      title: 'test2',
       content: 'test',
       createdAt: '23432341221',
     }
